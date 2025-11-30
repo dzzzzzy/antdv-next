70d329275265dfd8202be3c4b4c9aadcf0170da1
aibayanyu20 <aibayanyu@qq.com>
2025-11-30 14:41:10 +0800

    feat: add input demo

---
+++ playground/src/pages/input/demo/status.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ClockCircleOutlined } from '@antdv-next/icons'
+</script>
+
+<template>
+  <a-space direction="vertical" style="width: 100%;">
+    <a-input status="error" placeholder="Error" />
+    <a-input status="warning" placeholder="Warning" />
+    <a-input status="error" placeholder="Error with prefix">
+      <template #prefix>
+        <ClockCircleOutlined />
+      </template>
+    </a-input>
+    <a-input status="warning" placeholder="Warning with prefix">
+      <template #prefix>
+        <ClockCircleOutlined />
+      </template>
+    </a-input>
+  </a-space>
+</template>
